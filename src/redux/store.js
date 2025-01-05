4c259aab1a1e6fb193d775abb43f00209d1c60ec
Saiitbekuulu Maksatbek <sayitbekuulumaksat@gmail.com>
2025-01-06 00:54:38 +0600

    added errorSlice

--- src/redux/store.js
+++ src/redux/store.js
@@ -1,11 +1,13 @@
 import { configureStore } from "@reduxjs/toolkit";
 import reducer from "./slices/booksSlice";
 import filterSlice from "./slices/filterSlice";
+import errorSlice from "./slices/errorSlice";
 
 const store = configureStore({
   reducer: {
     books: reducer,
     filter: filterSlice,
+    error: errorSlice
   },
 });
 export default store;
